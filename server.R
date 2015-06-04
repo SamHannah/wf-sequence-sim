77963f980e0212a357357e7468fe5a680c347fac
SamHannah <SamHannah@users.noreply.github.com>
2015-06-04 11:33:45 -0600

    Update server.R
    minor tweaks

--- server.R
+++ server.R
@@ -30,11 +30,11 @@ shinyServer(
                                   legend.position="top")
       
       dp<- ggplot(outs[[1]], aes(Quartiles, Mean.deltaP, group= Test.Item, linetype=Test.Item, shape=Test.Item))+
-                        geom_line(size=1.0)+geom_point(size=4)+ylim(-0.35,0.35)+
-                        scale_shape_manual(name="HF Test item ", values=c(0,1))+
-                        scale_linetype_manual(name="HF Test item ", values=c(1,3))+geom_hline(aes(yintercept=0)) + 
-                        geom_errorbar(aes(ymin=Mean.deltaP-CI, ymax=Mean.deltaP+CI), width=0.15, lty=1)+
-                        labs(x="Trial Quartile", y=expression("Mean "~Delta~italic(P)~"('old')"))+themePro
+        geom_line(size=1.0)+geom_point(size=4)+ylim(min(outs[[1]]$Mean.delta-outs[[1]]$CI),
+        max(outs[[1]]$Mean.deltaP+outs[[1]]$CI))+scale_shape_manual(name="HF Test item ", values=c(0,1))+
+        scale_linetype_manual(name="HF Test item ", values=c(1,3))+geom_hline(aes(yintercept=0)) + 
+        geom_errorbar(aes(ymin=Mean.deltaP-CI, ymax=Mean.deltaP+CI), width=0.15, lty=1)+
+        labs(x="Trial Quartile", y=expression("Mean "~Delta~italic(P)~"('old')"))+themePro
       
       means<- ggplot(outs[[3]], aes(Test.Item, Mean.P.old))+geom_bar(stat="identity", fill=c("grey55", "black","black","grey55"))+
                         geom_errorbar(aes(ymin=Mean.P.old-SE, ymax=Mean.P.old+SE),width=0.15, 
@@ -42,8 +42,9 @@ shinyServer(
                         labs(x="Test Items", y="Mean P('old')")+themePro+theme(legend.position="none")
 
       crit<- ggplot(outs[[4]], aes(Trial, Criterion, group=Type,linetype=Type, colour=Type))+geom_line(size=1.0)+
-                        geom_hline(yintercept=0.5, size=1.0)+scale_linetype_manual(name="Initial criterion position",values=c(1,1))+
-                        scale_colour_manual(name="Initial criterion position", values=c("blue","red"))+ylim(0,1)+themePro
+        geom_hline(yintercept=0.5, size=1.0)+scale_linetype_manual(name="Initial criterion position",values=c(1,1))+
+        scale_colour_manual(name="Initial criterion position", values=c("blue","red"))+ylim(min(outs[[4]]$Criterion-.05),
+        max(outs[[4]]$Criterion+0.05))+themePro
       
       outs[[5]]$Test.Item <- as.factor(outs[[5]]$Test.Item)
       outs[[5]]$Test.Item <- factor(outs[[5]]$Test.Item, levels=c("LF Lure", "HF Lure","HF Target","LF Target" ))
